27a63ba9768a5cd2f7be1002dfc76b3f5978ae59
viktorpolak98 <vigge.polak98@gmail.com>
2022-12-10 13:40:51 +0100

    Create LeaderCalculator.cs

---
+++ WebApp/Logic/LeaderCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using WebApp.Repositories;
+
+namespace WebApp.Logic
+{
+    public class LeaderCalculator
+    {
+        private PoliceEventsRepository eventsRepository;
+        private PoliceStationsRepository stationsRepository;
+
+    }
+}
