203613f8c702e644426462c69bd7673c256428dc
viktorpolak98 <vigge.polak98@gmail.com>
2024-04-27 18:37:53 +0200

    Fixed styling

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Text.Json;
 using WebApp.Repositories;
@@ -7,7 +8,7 @@ using WebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddHttpClient<IReadData<JsonDocument>, PoliceAPICaller>(client =>
 {
@@ -18,16 +19,25 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
 app.UseHttpsRedirection();
 
 app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-    endpoints.MapRazorPages();
-});
+
+app.MapControllers();
+app.MapRazorPages();
+
+app.UseStaticFiles();
 
 app.Run();
