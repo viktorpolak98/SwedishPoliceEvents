b3f0573696faefc93266ca5fde7872ff422bb37f
viktorpolak98 <vigge.polak98@gmail.com>
2024-04-28 21:11:53 +0200

    Added pages and class for event in list

---
+++ WebApp/Pages/Stations.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class StationsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+export class ListEvent {
+    constructor(title, city) {
+        this.title = title;
+        this.city = city;
+    }
+}
