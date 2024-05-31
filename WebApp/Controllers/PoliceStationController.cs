539b58b0b91e468aa1b7cf12486569b5cd979da4
viktorpolak98 <vigge.polak98@gmail.com>
2024-05-31 18:56:59 +0200

    Implemented stationController

--- WebApp/Controllers/PoliceStationController.cs
+++ WebApp/Controllers/PoliceStationController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Repositories;
 
 namespace WebApp.Controllers
 {
@@ -6,25 +7,45 @@ namespace WebApp.Controllers
     [Route("/PoliceStation")]
     public class PoliceStationController : Controller
     {
+        private readonly PoliceStationsRepository _repository = new();
+        private readonly PoliceAPICaller _apiCaller;
+        //TODO: Change to real path
+        private readonly string path = "";
+
         [HttpGet]
         [Route("GetAllPoliceStations")]
         public IActionResult GetAllPoliceStations()
         {
-            return Ok("stuff");
+            if (!_repository.CacheIsFull())
+            {
+                _repository.CreateValues(_apiCaller.ReadData(path).Result);
+            }
+
+            return Ok(_repository.GetAll());
         }
 
         [HttpGet]
         [Route("GetPoliceStationsByLocation")]
         public IActionResult GetPoliceStationsByLocation(string location)
         {
-            return Ok("stuff");
+            if (!_repository.CacheIsFull())
+            {
+                _repository.CreateValues(_apiCaller.ReadData(path).Result);
+            }
+
+            return Ok(_repository.GetAllByLocationName(location));
         }
 
         [HttpGet]
         [Route("GetPoliceStationsByService")]
         public IActionResult GetPoliceStationsByService(string service)
         {
-            return Ok("stuff");
+            if (!_repository.CacheIsFull())
+            {
+                _repository.CreateValues(_apiCaller.ReadData(path).Result);
+            }
+
+            return Ok(_repository.GetAllByDisplayName(service));
         }
     }
 }
