c8d960beffcc6d1f16d3462994fd486d44f75618
viktorpolak98 <vigge.polak98@gmail.com>
2024-06-04 19:55:34 +0200

    Added class to avoid recreating every item on data fetch

---
+++ WebApp/Models/Shared/ItemContainer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace WebApp.Models.Shared
+{
+    public class ItemContainer <T>
+    {
+        public LinkedList<T> Items { get; set; }
+        public int MaxNumberOfItems { get; } 
+
+        public ItemContainer(int maxNumberOfItems)
+        {
+            Items = new LinkedList<T>();
+            MaxNumberOfItems = maxNumberOfItems;
+        }
+
+        public void AddItem(T item)
+        {
+            if(Items.Count >= MaxNumberOfItems)
+            {
+                Items.RemoveLast();
+            }
+
+            Items.AddFirst(item);
+        }
+
+    }
+}
