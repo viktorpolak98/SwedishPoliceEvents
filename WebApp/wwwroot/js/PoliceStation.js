ecc655094c3d63ecf835162fce4d349efa174570
viktorpolak98 <vigge.polak98@gmail.com>
2024-08-17 20:09:56 +0200

    Added function for fetching stations

--- WebApp/wwwroot/js/PoliceStation.js
+++ WebApp/wwwroot/js/PoliceStation.js
@@ -14,3 +14,29 @@ const mapOfStations = new Map();
 function addStationToMap(PoliceStation) {
     mapOfStations.set(PoliceStation.id, PoliceStation);
 }
+
+export async function getAllStations() {
+    const url = `${location.origin}/PoliceStations/GetAllPoliceStations`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        mapOfStations.clear();
+        const data = await response.json();
+        data.forEach(item => {
+            const station = new PoliceStation(
+                item.id,
+                item.name,
+                item.url,
+                item.location.name,
+                `${item.location.gpsLocation.latitude},${item.location.gpsLocation.longitude}`,
+                item.services
+            );
+            addStationToMap(station);
+        });
+    } catch (error) {
+        console.error('Error fetching police stations', error);
+    }
+}
