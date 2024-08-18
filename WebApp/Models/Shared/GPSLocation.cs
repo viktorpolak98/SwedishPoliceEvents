98541b11510af65f7ae95110ccaeb2b1e7f0f46c
viktorpolak98 <vigge.polak98@gmail.com>
2024-08-18 22:07:26 +0200

    Changed order of lat-long

--- WebApp/Models/Shared/GPSLocation.cs
+++ WebApp/Models/Shared/GPSLocation.cs
@@ -2,8 +2,8 @@
 
 public class GPSLocation
 {
-    public string Longitude { get; set; }
     public string Latitude { get; set; }
+    public string Longitude { get; set; }
 
     public override string ToString()
     {
